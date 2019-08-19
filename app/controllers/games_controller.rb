fad4446ef69e3c116e39c46420a1b9f6dfc8f245
Guilherme Esteves <guiesteves1985@gmail.com>
2019-08-19 15:19:19 -0300

    part 1 exercise 3

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,9 @@
+class GamesController < ApplicationController
+  def score
+  end
+
+  def new
+    # The new action will be used to display a new random grid and a form.
+    # The form will be submitted (with POST) to the score action.
+  end
+end
